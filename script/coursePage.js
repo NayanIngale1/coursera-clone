2c17c875672a9d8f050f94a7e06948dcda18b171
NayanIngale1 <nayaningale15@gmail.com>
2022-02-25 13:25:14 +0530

    navbar and footer is imported

--- script/coursePage.js
+++ script/coursePage.js
@@ -1,3 +1,10 @@
+import navbar from "../components/navbar.js";
+import { Minifooter } from "../components/footer.js";
+let navBar = document.getElementById("navbar");
+navBar.innerHTML = navbar();
+let footerDiv = document.getElementById("footer_div");
+footerDiv.innerHTML = Minifooter();
+
 const faqs = document.querySelectorAll(".faq");
 faqs.forEach((faq) => {
   faq.addEventListener("click", () => {
