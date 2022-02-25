c72f08633eeb4043216149a3e17c2f3fc8cb1c40
Ajinkya Joshi <95956960+Ajinkya6898@users.noreply.github.com>
2022-02-25 15:39:32 +0530

    End

---
+++ Ajinkya/components/footer.js
@@ -0,0 +1,132 @@
+const footer = ()=>{
+    return ` <div id="jfooter">
+    
+
+    <div class="footer_list">
+        <h2>Coursera</h2>
+        <p><a href="">About</a></p>
+        <p><a href="">What We Offer</a></p>
+        <p><a href="">Leadership</a></p>
+        <p><a href="">Careers</a></p>
+        <p><a href="">Catalog</a></p>
+        <p><a href="">Coursera Plus</a></p>
+        <p><a href="">Professional Certificates</a></p>
+        <p><a href="">MasterTrack® Certificates</a></p>
+        <p><a href="">Degrees</a></p>
+        <p><a href="">For Enterprise</a></p>
+        <p><a href="">For Government</a></p>
+        <p><a href="">For Campus</a></p>
+        <p><a href="">Become a Partner</a></p>
+        <p><a href="">Coronavirus Response</a></p>
+    </div>
+    <div class="footer_list">
+        <h2>Community</h2>
+        <p><a href="">Learners</a></p>
+        <p><a href="">Partners</a></p>
+        <p><a href="">Developers</a></p>
+        <p><a href="">Beta Testers</a></p>
+        <p><a href="">Translators</a></p>
+        <p><a href="">Blog</a></p>
+        <p><a href="">Tech Blog</a></p>
+        <p><a href="">Teaching Center</a></p>
+    </div>
+    <div class="footer_list">
+        <h2>More</h2>
+        <p><a href="">Press</a></p>
+        <p><a href="">Investors</a></p>
+        <p><a href="">Terms</a></p>
+        <p><a href="">Privacy</a></p>
+        <p><a href="">Help</a></p>
+        <p><a href="">Accessibility</a></p>
+        <p><a href="">Contact</a></p>
+        <p><a href="">Articles</a></p>
+        <p><a href="">Directory</a></p>
+        <p><a href="">Affiliates</a></p>
+    </div>
+    <div class="last_footer_list">
+        <!-- <h2>Learn Anywhere</h2> -->
+        <img src="./components/icons/download_on_the_app_store_badge_en.svg" alt="">
+        <img src="./components/icons/en_generic_rgb_wo_45.png" alt="">
+        <img id="certified" src="./components/icons/2018-B-Corp-Logo-Black-S.png" alt="">
+    </div>
+</div>
+<div id="last_part_of_footer">
+        <p>© 2022 Coursera Inc. All rights reserved.</p>
+        <div>
+            <img src="./components/icons/facebook.png" alt="">
+            <img src="./components/icons/linkedin.png" alt="">
+            <img src="./components/icons/twitter.png" alt="">
+            <img src="./components/icons/youtube.png" alt="">
+            <img src="./components/icons/instagram.png" alt="">
+        </div>
+    </div>`;
+};
+
+
+
+const Minifooter = ()=>{
+    return ` <div id="jfooter">
+    
+    <div class="footer_list">
+        <h2>Coursera</h2>
+        <p><a href="">About</a></p>
+        <p><a href="">What We Offer</a></p>
+        <p><a href="">Leadership</a></p>
+        <p><a href="">Careers</a></p>
+        <p><a href="">Catalog</a></p>
+        <p><a href="">Coursera Plus</a></p>
+        <p><a href="">Professional Certificates</a></p>
+        <p><a href="">MasterTrack® Certificates</a></p>
+        <p><a href="">Degrees</a></p>
+        <p><a href="">For Enterprise</a></p>
+        <p><a href="">For Government</a></p>
+        <p><a href="">For Campus</a></p>
+        <p><a href="">Become a Partner</a></p>
+        <p><a href="">Coronavirus Response</a></p>
+    </div>
+    <div class="footer_list">
+        <h2>Community</h2>
+        <p><a href="">Learners</a></p>
+        <p><a href="">Partners</a></p>
+        <p><a href="">Developers</a></p>
+        <p><a href="">Beta Testers</a></p>
+        <p><a href="">Translators</a></p>
+        <p><a href="">Blog</a></p>
+        <p><a href="">Tech Blog</a></p>
+        <p><a href="">Teaching Center</a></p>
+    </div>
+    <div class="footer_list">
+        <h2>More</h2>
+        <p><a href="">Press</a></p>
+        <p><a href="">Investors</a></p>
+        <p><a href="">Terms</a></p>
+        <p><a href="">Privacy</a></p>
+        <p><a href="">Help</a></p>
+        <p><a href="">Accessibility</a></p>
+        <p><a href="">Contact</a></p>
+        <p><a href="">Articles</a></p>
+        <p><a href="">Directory</a></p>
+        <p><a href="">Affiliates</a></p>
+    </div>
+    <div class="last_footer_list">
+        <!-- <h2>Learn Anywhere</h2> -->
+        <img src="./components/icons/download_on_the_app_store_badge_en.svg" alt="">
+        <img src="./components/icons/en_generic_rgb_wo_45.png" alt="">
+        <img id="certified" src="./components/icons/2018-B-Corp-Logo-Black-S.png" alt="">
+    </div>
+</div>
+<div id="last_part_of_footer">
+        <p>© 2022 Coursera Inc. All rights reserved.</p>
+        <div>
+            <img src="./components/icons/facebook.png" alt="">
+            <img src="./components/icons/linkedin.png" alt="">
+            <img src="./components/icons/twitter.png" alt="">
+            <img src="./components/icons/youtube.png" alt="">
+            <img src="./components/icons/instagram.png" alt="">
+        </div>
+    </div>`;
+};
+
+
+
+export {footer,Minifooter  };
